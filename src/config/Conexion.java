8a429175635d27d5ccac128cef0f4d66406e72b6
Saul MzPz <saul.mzpz@gmail.com>
2022-05-26 12:57:00 -0500

    Commit inicial, listo boton agregar

---
+++ src/config/Conexion.java
@@ -0,0 +1,24 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class Conexion {
+    Connection con;
+    
+    public Conexion(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tablasj","root","");
+        }catch (Exception e){
+            System.err.println("error" +e);
+        }
+            
+    }
+    
+    public Connection getConnection(){
+    return con;
+    }
+    
+}
